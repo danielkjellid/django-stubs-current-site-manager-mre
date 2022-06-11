88964fad56aee3c1b28d2a5c48f6fb6e58b76b17
danielkjellid <daniel.kjellid@oda.com>
2022-06-11 19:24:50 +0200

    Initial commit

---
+++ myproject/myapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.sites.models import Site
+from django.contrib.sites.managers import CurrentSiteManager
+
+class MyModel(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+
+    on_site = CurrentSiteManager()
